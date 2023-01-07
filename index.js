8da87db6c69044d4c3d988f82c5d654c35ef73e0
mccoyjimi <83176638+mccoyjimi@users.noreply.github.com>
2023-01-08 00:35:54 +0100

    footer added footer not yet optimized

---
+++ index.js
@@ -0,0 +1,30 @@
+//variables
+var name = "jimmy";
+//to display  the variable, use console log function
+console.log(name);
+//variables
+var name1 = "jimmy";
+//to display  the variable, use console log function
+console.log(name1);
+//rules in naming variables
+//should be meaningfull
+//don't use reserve keywords eg. if,else,var
+//variable name cannot start with numbers
+//variable name cannot contain space or hyphen....use underscore(snake_case)
+var date_of_birth = "1998";
+console.log(date_of_birth);
+
+var age = "24";
+var gender = "male";
+console.log(name1, gender, age);
+
+//DECLARE VARIABLES AS LET & CONSTANT
+let price = 250;
+const name2 = "mccoy";
+console.log(price, name2);
+//you can edit (reassign the value) in the let variable but not the const
+let price = 250;
+const name2 = "mccoy";
+console.log(price, name2);
+price = 300;
+console.log(price);
